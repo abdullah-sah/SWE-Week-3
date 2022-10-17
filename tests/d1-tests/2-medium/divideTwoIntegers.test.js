cd6c8b02d242e29197e378e006a91a62e5b34986
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-10-17 16:51:36 +0100

    Completed d1 easy and medium tasks.

--- tests/d1-tests/2-medium/divideTwoIntegers.test.js
+++ tests/d1-tests/2-medium/divideTwoIntegers.test.js
@@ -1,9 +1,31 @@
 const {divideTwoIntegers} = require('../../../d1-tests/2-medium/divideTwoIntegers')
 
 //write the following tests
+describe('checking if dividTwoIntegers functions as expected', () => {
     // checks if divideTwoIntegers receiving 12 and 3 will return 4
+    test('checks if divideTwoIntegers receiving 12 and 3 will return 4', () => {
+        expect(divideTwoIntegers(12, 3)).toBe(4);
+    });
+    
     // checks if divideTwoIntegers receiving 12 and -3 will return -4
+    test('checks if divideTwoIntegers receiving 12 and -3 will return -4', () => {
+        expect(divideTwoIntegers(12, -3)).toBe(-4);
+    });
+
     // checks if divideTwoIntegers receiving 12 and 0 will return "Divide by zero error."
+    test('checks if divideTwoIntegers receiving 12 and 0 will return "Divide by zero error."', () => {
+        expect(divideTwoIntegers(12, 0)).toBe('Divide by zero error.');
+    });
+
     // if divideTwoIntegers is passed a value other than a number, it should return "Inputs must be a number"
+    test('if divideTwoIntegers is passed a value other than a number, it should return "Inputs must be a number', () => {
+        expect(divideTwoIntegers(12, 'something')).toBe('Inputs must be a number.');
+    });
+
     // if divideTwoIntegers is passed a decimal value, it should return "Integers only."
+    test('if divideTwoIntegers is passed a decimal value, it should return "Integers only"', () => {
+        expect(divideTwoIntegers(12, 23.4)).toBe('Integers only.');
+    });
+
+})
     
--- tests/d1-tests/3-hard/addingArrays.test.js
+++ tests/d1-tests/3-hard/addingArrays.test.js
@@ -11,3 +11,34 @@ Write tests for the following:
     - two arrays of numbers and the expected result
 
 */
+
+expect.extend({
+    toReturnTheArray(received, expected) {
+        const message = `Expecting ${received} to equal ${expected}.`;
+        if (JSON.stringify(received) === JSON.stringify(expected)) {
+            return {
+                pass: true,
+                message
+            }
+        } else {
+            return {
+                pass: false,
+                message
+            }
+        }
+    }
+})
+
+describe('testing to make sure addingArrays functions as expected', () => {
+    test('Arrays with different lengths should return "Arrays are different lengths."', () => {
+        expect(addingArrays([1, 2, 3], [1, 2, 3, 4])).toEqual('Arrays are different lengths.');
+    });
+
+    test('Arrays with non-numbers should return "Not all elements are numbers."', () => {
+        expect(addingArrays([1, '2', 3], [1, 2, 3])).toBe('Not all elements are numbers.');
+    });
+
+    test('Given arrays [10, 11, 12] and [10, 11, 12], the result should be [20, 22, 24].', () => {
+        expect(addingArrays([10, 11, 12], [10, 11, 12])).toReturnTheArray(addingArrays([10, 11, 12], [10, 11, 12]), [20, 22, 24]);
+    });
+})

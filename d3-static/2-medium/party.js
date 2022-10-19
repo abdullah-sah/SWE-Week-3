7a8f9997e8734e39088ee59b3dcf1f5f7a76eedf
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-10-19 18:14:42 +0100

    Started on day 3 medium challenge.

--- d3-static/2-medium/party.js
+++ d3-static/2-medium/party.js
@@ -1,9 +1,19 @@
 const {Character} = require('../1-easy/static')
-class Party {
+class Party extends Character {
   
-    
+	constructor(name) {
+		super(name);
+		this.partyMembers = {
+			leader: Party.leader,
+			members: [name]
+		}
+	}
 
+	static leader = this.name;
+	static members = this.partyMembers;
 }
 
+const p = new Party('boby');
+console.log(Party.leader);
 
 module.exports = {Party, Character}

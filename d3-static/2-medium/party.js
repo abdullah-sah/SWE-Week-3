aba12daa143119c5d67031ee77cdb7650867463a
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-10-19 21:41:08 +0100

    Fixed easy task + completed medium.

--- d3-static/2-medium/party.js
+++ d3-static/2-medium/party.js
@@ -1,37 +1,37 @@
 const {Character} = require('../1-easy/static')
-<<<<<<< HEAD
-class Party extends Character {
-  
-	constructor(name) {
-		super(name);
-		this.partyMembers = {
-			leader: Party.leader,
-			members: [name]
-		}
-	}
-
-	static leader = this.name;
-	static members = this.partyMembers;
-}
-=======
 /*
 class defined as Party
-  Constructor 
-    @param {object} leader - this object is expected to be an instance of the character class.
-    
-    constructor should throw the following error 'A party needs a leaader!' if no leader object is provided.
-    constructor should throw the following error '${leader.name} is already leading a party.' if the character is used more than once.
-    a leader property that includes the leader's name.
-    a members property that is an array, with the first element being the leader object.
+	Constructor 
+		@param {object} leader - this object is expected to be an instance of the character class.
+		
+		constructor should throw the following error 'A party needs a leaader!' if no leader object is provided.
+		constructor should throw the following error '${leader.name} is already leading a party.' if the character is used more than once.
+		a leader property that includes the leader's name.
+		a members property that is an array, with the first element being the leader object.
 
-    Static Properties
-        totalParties - keeps a running count of parties created
-        currentLeaders - an array of the names of leaders of each created party.  (this will help with throwing the above errors)
+		Static Properties
+				totalParties - keeps a running count of parties created
+				currentLeaders - an array of the names of leaders of each created party.  (this will help with throwing the above errors)
 
 */
->>>>>>> 39ed648f84c5ec476871b2834b3069093e9b4f65
 
-const p = new Party('boby');
-console.log(Party.leader);
+
+class Party {
+	constructor(leader) {
+		if (typeof leader === 'undefined') {
+			throw new Error('A party needs a leader!');
+		} else if (Party.currentLeaders.includes(leader.name)) {
+			throw new Error(`${leader.name} is already leading a party.`);
+		}
+
+		this.leader = leader.name;
+		this.members = [leader];
+		Party.totalParties++;
+		Party.currentLeaders.push(leader.name);
+	}
+
+	static totalParties = 0;
+	static currentLeaders = [];
+}
 
 module.exports = {Party, Character}
